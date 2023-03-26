8b55203c100755714ff45d1e19aeecd7a66766e5
ezmore <125889905+ezmore@users.noreply.github.com>
2023-03-26 09:36:21 +0800

    Create readme.cmd

---
+++ readme.cmd
@@ -0,0 +1,5 @@
+1、学习直播的搭建和应用，最初的想法来自于电丸科技AK。
+FFmpeg方法的视频地址：https://www.youtube.com/watch?v=Ko20sPb93fo&list=PLNb-Mjqdw92V9K_aA8W_IcKDYneNKq0FB&index=5&t=1743s；
+lala大神的说明及脚本地址：https://lala.im/4816.html
+ffm.sh为lala大神的代码
+.Log /2023.3.26
